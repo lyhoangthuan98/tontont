6be26700c92b3cd365d98e6879614c1d68e155d8
lyhoangthuan98 <46866770+lyhoangthuan98@users.noreply.github.com>
2019-03-15 20:15:17 +0700

    up

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** Thiết lập MySQL - Bạn có thể lấy các thông tin này từ host/server ** //
 /** Tên database MySQL */
-define('DB_NAME', 'ten_database');
+define('DB_NAME', 'uirl8kbn44j1r8ym	');
 
 /** Username của database */
-define('DB_USER', 'username');
+define('DB_USER', 'di1ktdy1swgd6x6g');
 
 /** Mật khẩu của database */
-define('DB_PASSWORD', 'mat_khau');
+define('DB_PASSWORD', 'ig9d0frwo79vv5gw');
 
 /** Hostname của database */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'u615qyjzybll9lrm.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
 
 /** Database charset sử dụng để tạo bảng database. */
 define('DB_CHARSET', 'utf8');
@@ -48,14 +48,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'khóa không trùng nhau');
-define('SECURE_AUTH_KEY',  'khóa không trùng nhau');
-define('LOGGED_IN_KEY',    'khóa không trùng nhau');
-define('NONCE_KEY',        'khóa không trùng nhau');
-define('AUTH_SALT',        'khóa không trùng nhau');
-define('SECURE_AUTH_SALT', 'khóa không trùng nhau');
-define('LOGGED_IN_SALT',   'khóa không trùng nhau');
-define('NONCE_SALT',       'khóa không trùng nhau');
+define('AUTH_KEY',         '4=m5kk?M^`Q_ub<0.hK|m|0B|q{n|A4?,t|rVL>{4PaSa5_XW^qU9Wg|.i!>-S`5');
+define('SECURE_AUTH_KEY',  'ZiqQ&qqneeJ)2hp-QD2kzIa/D|Uj|2vCbb5^tiqmie~XV|5*B&/+;hkm(fD,j4yY');
+define('LOGGED_IN_KEY',    'Qcx#=O`oD`**4ciDt$>7f`0 C(YJBG zw&i6Zs?rD<av+&&LcbFzn%OZX0F!z_-Y');
+define('NONCE_KEY',        ' +a|e5G<$uYyZ@I-x%pcX3MerOW!$?<{im-1i30{S=RGJ,y<-[4P2qtC^+xBAUFs');
+define('AUTH_SALT',        '?UB,xiF#o`KZlz7bj]R$u_cj+X;mE,Z`zI-J!&mKlA%6i+N@vx#Aav|-/u,U!m(F');
+define('SECURE_AUTH_SALT', '[,KDOIVx!k|2N*&p5sGYn<?X oBC@] Lda@=!l*Zb7@.eT5 )j#|/=*XO7WXB:_y');
+define('LOGGED_IN_SALT',   'S]_m7|xhw!A`|Ja1zVfr:Q,)HyZ.o+Xsh4$~`:Q%<j|a-Jet7^J_h-1.jw,,kh|8');
+define('NONCE_SALT',       '7M&bbYX52-/r;.DGqN:l-S/vdQb5X:`+E/5Bw]M]&W/lx?G3R2tMA8>m^T#+.-`9');
 
 /**#@-*/
 
